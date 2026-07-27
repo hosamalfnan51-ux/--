c50ea4efaf50488001d1426dffdf5260b914e956
hosamalfnan51-ux <hosamalfnan51@gmail.com>
2026-07-27 16:25:02 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,12 +11,13 @@ plugins {
 
 android {
   namespace = "com.example"
-  compileSdk { version = release(36) { minorApiLevel = 1 } }
+  compileSdk = 36
 
   defaultConfig {
     applicationId = "com.aistudio.quranway.gbtryp"
     minSdk = 24
     targetSdk = 36
+
     versionCode = 1
     versionName = "1.0"
 
